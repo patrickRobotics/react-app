51c7a2927af9a1fdb0e77fb8b3e0490401863a1a
patrickRobotics <pirungu9@gmail.com>
2021-10-01 11:31:59 +0300

    add new task form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [ tasks, setTasks ] = useState([
@@ -25,7 +26,13 @@ function App() {
     }
   ])
 
-  // Delete
+  // Add Task
+  const addTask = (task) => {
+    const id  = Math.floor(Math.random() * 1000 ) + 1
+    const newTask = { id, ...task }
+    setTasks([ ...tasks, newTask ])
+  }
+  // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id ))
   }
@@ -37,6 +44,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks 
           tasks={ tasks }
